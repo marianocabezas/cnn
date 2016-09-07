45c1d1e1c78b867932fc3754aaed823a9c197f83
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-09-07 17:02:26 +0200

    - Changed debug print

--- data_creation.py
+++ data_creation.py
@@ -221,7 +221,11 @@ def get_norm_patch_vectors(image_names, positive_masks, negative_masks, size, ra
     negative_patches = get_list_of_patches(images, negative_centers, size)
 
     # Return the patch vectors
-    data = [np.concatenate([p1, p2]) for p1, p2 in zip(positive_patches, negative_patches) if p1 and p2]
+    for p1, p2 in zip(positive_patches, negative_patches):
+        if p1 and p2:
+            print('%d = %s' % (len(p1), ','.join(['-'.join([str(length) for length in patch.shape]) for patch in p1])))
+            print('%d = %s' % (len(p2), ','.join(['-'.join([str(length) for length in patch.shape]) for patch in p2])))
+    data = [np.concatenate([p1, p2]) for p1, p2 in zip(positive_patches, negative_patches)]
     return data
 
 
