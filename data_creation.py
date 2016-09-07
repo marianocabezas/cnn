f1e6da123eaae8fc5a12b75ec9a5fe0884ad8ec7
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-09-07 14:10:10 +0200

    - Debugging again

--- data_creation.py
+++ data_creation.py
@@ -187,7 +187,7 @@ def get_list_of_patches(image_list, center_list, size):
     i = 0
     for image, centers in zip(image_list, center_list):
         print('Processing image %d' % i)
-        print('Centers = (%d,%s)' % len(centers), '+'.join([str(len(center)) for center in centers]))
+        print('Centers = (%d,%s)' % (len(centers), '+'.join([str(len(center)) for center in centers])))
         i += 1
         np.array(get_patches(image, centers, size))
     return [np.array(get_patches(image, centers, size)) for image, centers in zip(image_list, center_list)]
