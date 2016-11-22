0d939adacf06c3cbda73f310400ae1b5084a1cd3
Mariano Cabezas <mcabezas@eia.udg.edu>
2016-11-22 16:19:22 +0100

    - Added parameters to the command line "command"

--- train_register_net.py
+++ train_register_net.py
@@ -74,7 +74,7 @@ def main():
 
     seed = np.random.randint(np.iinfo(np.int32).max)
 
-    input_size_s = 'x'.join(input_size)
+    input_size_s = 'x'.join(str(input_size))
     sufix = '.s%s.c%s.n%s' % (input_size_s, conv_width, n_filters)
     net_name = os.path.join(dir_name, 'deep-exp_registration.' + sufix + '.')
     net = create_cnn3d_register(
