4b7f2a7923149bb457fe510fde6427df4c9e3703
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-02-27 18:14:08 +0100

    - Added a WeightLogger to keep track of the weight during training. - Added functions to plot CNN architectures. Right now, I don't keep track of unique layers, so some of them are repeated. My goal is to fix that.

--- utils.py
+++ utils.py
@@ -1,18 +1,106 @@
 import numpy as np
+from lasagne.layers import InputLayer
+import re
 from math import floor
+import pickle
 
 
 def color_codes():
-    codes = {'g': '\033[32m',
-             'c': '\033[36m',
-             'bg': '\033[32;1m',
-             'b': '\033[1m',
-             'nc': '\033[0m',
-             'gc': '\033[32m;0m',
-             'lgy': '\033[37m',
-             'dgy': '\033[30m',
-             }
+    codes = {
+        'nc': '\033[0m',
+        'b': '\033[1m',
+        'k': '\033[0m',
+        '0.25': '\033[30m',
+        'dgy': '\033[30m',
+        'r': '\033[31m',
+        'g': '\033[32m',
+        'gc': '\033[32m;0m',
+        'bg': '\033[32;1m',
+        'y': '\033[33m',
+        'c': '\033[36m',
+        '0.75': '\033[37m',
+        'lgy': '\033[37m',
+    }
     return codes
+
+
+def inverse_color_codes():
+    codes = {
+        '\033[30m': '0.25',
+        '\033[31m': 'r',
+        '\033[32m': 'g',
+        '\033[33m': 'y',
+        '\033[34m': 'b',
+        '\033[35m': 'm',
+        '\033[36m': 'c',
+        '\033[37m': '0.75',
+    }
+    return codes
+
+
+def name_and_color(layer_name):
+    color_match = re.search('\x1b\[([0-9]+;*[0-9]*)m', layer_name) if layer_name else None
+    color = inverse_color_codes()[color_match.group(0)] if color_match else 'k'
+    name = ''.join(re.split('\x1b\[[0-9]+;*[0-9]*m', layer_name)) if layer_name else ''
+
+    return color, name
+
+
+def get_layer_depth(layer):
+    if isinstance(layer, InputLayer):
+        return 0
+    else:
+        try:
+            inputs = layer.input_layers
+        except AttributeError:
+            inputs = [layer.input_layer]
+    return max([get_layer_depth(l) for l in inputs]) + 1
+
+
+def plot_layer(layer, xmin, xmax, ymax, height, fontsize, fig):
+    color, name = name_and_color(layer.name)
+    c = color_codes()
+    print(c[color] + '%s ([%f-%f]: %f)' % (name, xmin, xmax, (xmax + xmin) / 2.0) + c['nc'])
+    fig.text(
+        (xmax + xmin)/2.0,
+        (ymax - get_layer_depth(layer) + 1) / height,
+        name,
+        ha='center',
+        va='center',
+        color=color,
+        size=fontsize,
+        transform=fig.transFigure,
+        bbox=dict(boxstyle='round', fc='w', ec='k')
+    )
+
+    if not isinstance(layer, InputLayer):
+        try:
+            inputs = layer.input_layers
+        except AttributeError:
+            inputs = [layer.input_layer]
+
+        inputs_width = float((xmax - xmin))/len(inputs)
+        inputs_xmin = [x * inputs_width for x in range(len(inputs))]
+        inputs_xmax = [(x + 1.0) * inputs_width for x in range(len(inputs))]
+        for xmin_i, xmax_i, i in zip(inputs_xmin, inputs_xmax, inputs):
+            plot_layer(i, xmin + xmin_i, xmin + xmax_i, ymax, height, fontsize, fig)
+
+
+def plot_layer_tree(final_layer):
+    import matplotlib
+    matplotlib.use('Qt4Agg')
+    import matplotlib.pyplot as plt
+    depth = get_layer_depth(final_layer)
+    spacing = 1.2
+    height = (spacing * (depth + 1) + .5)
+    fontsize = 0.3 * 72
+
+    fig = plt.figure(1, ((3 * height / 4) / 1.5, height / 1.5))
+
+    plot_layer(final_layer, 0, 1, depth, height, fontsize, fig)
+
+    plt.draw()
+    plt.show()
 
 
 def random_affine3d_matrix(x_range=np.pi, y_range=np.pi, z_range=np.pi, t_range=5):
@@ -91,6 +179,24 @@ class EarlyStopping(object):
             print('Early stopping.')
             print('Best valid loss was {:.6f} at epoch {}.'.format(
                 self.best_valid, self.best_valid_epoch))
-            # nn.load_weights_from(self.best_weights)
             nn.get_all_params_values()
             raise StopIteration()
+
+
+class WeightsLogger(object):
+    """From https://github.com/dnouri/kfkd-tutorial"""
+    def __init__(self, filename):
+        self.params = []
+        self.filename = filename
+
+    def __call__(self, nn, train_history):
+        params = dict(
+            train=train_history[-1]['train_loss'],
+            valids=train_history[-1]['valid_loss'],
+            weights=nn.get_all_params()
+        )
+        self.params.append(params)
+
+    def save(self):
+        with open(self.filename, 'wb') as f:
+            pickle.dump(self.params, f, -1)
