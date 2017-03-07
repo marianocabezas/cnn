183f05d720638f8d52807b37f371c38f2101c217
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-03-07 17:18:03 +0100

    - While I might not be using "binary" objective functions soon, I adapted all functions to different outputs (multiclass or binary)

--- objective_functions.py
+++ objective_functions.py
@@ -1,34 +1,39 @@
-from theano import tensor
+from theano.tensor import sum, mean, std, log
 import numpy as np
 
 
 def probabilistic_dsc_objective(predictions, targets):
-    top = 2 * tensor.sum(predictions[:, 1] * targets)
-    bottom = tensor.sum(predictions[:, 1]) + tensor.sum(targets)
+    multi = len(predictions.shape) > 1 and predictions.shape[1] > 1
+    top = 2 * sum(predictions[:, 1] * targets) if multi else 2 * sum(predictions * targets)
+    bottom = sum(predictions[:, 1]) + sum(targets) if multi else sum(predictions) + sum(targets)
     return 1-(top / bottom)
 
 
 def cross_correlation(x, y):
-    x_mean = tensor.mean(x)
-    y_mean = tensor.mean(y)
-    x_stdev = tensor.std(x)
-    y_stdev = tensor.std(y)
+    x_mean = mean(x)
+    y_mean = mean(y)
+    x_stdev = std(x)
+    y_stdev = std(y)
     y_dev = y - y_mean
     x_dev = x - x_mean
-    return 1 - (tensor.mean(x_dev*y_dev / (x_stdev*y_stdev)))
+    return 1 - (mean(x_dev*y_dev / (x_stdev*y_stdev)))
 
 
 def logarithmic_dsc_objective(predictions, targets):
-    top = tensor.log(2) + tensor.log(tensor.sum(predictions[:, 1] * targets))
-    bottom = tensor.log(tensor.sum(predictions[:, 1]) + tensor.sum(targets))
+    top = log(2) + log(sum(predictions[:, 1] * targets))
+    bottom = log(sum(predictions[:, 1]) + sum(targets))
     return -(top - bottom)
 
 
 def accuracy_dsc_probabilistic(target, estimated):
-    return 2 * np.sum(target * estimated[:, 1]) / (np.sum(target) + np.sum(estimated[:, 1]))
+    multi = len(estimated.shape) > 1 and estimated.shape[1] > 1
+    top = 2 * np.sum(target * estimated[:, 1]) if multi else 2 * np.sum(target * estimated)
+    bottom = np.sum(target) + np.sum(estimated[:, 1]) if multi else np.sum(target) + np.sum(estimated)
+    return top / bottom
 
 
 def accuracy_dsc(target, estimated):
+    multi = len(estimated.shape) > 1 and estimated.shape[1] > 1
     a = target.astype(dtype=np.bool)
-    b = np.array(estimated[:, 1] > 0.8).astype(dtype=np.bool)
+    b = np.array(estimated[:, 1] > 0.8) if multi else np.array(estimated > 0.8)
     return 2 * np.sum(np.logical_and(a, b)) / np.sum(np.sum(a) + np.sum(b))
